9a1101e4e72f611ab84c8cf3f450461946605d9b
”Andrei” <andrei.chirila07121999@gmail.com>
2026-09-14 00:48:27 +0300

    feat: add category entity

---
+++ src/main/java/dev/andrei/chirila/lowroll/category/Category.java
@@ -0,0 +1,33 @@
+package dev.andrei.chirila.lowroll.category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
