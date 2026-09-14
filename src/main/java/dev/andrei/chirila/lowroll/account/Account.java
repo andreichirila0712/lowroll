ee078ed726706bfe65a7c53be4e9dd6a0e21bca6
”Andrei” <andrei.chirila07121999@gmail.com>
2026-09-14 14:03:21 +0300

    feat(account): add account entity

---
+++ src/main/java/dev/andrei/chirila/lowroll/account/Account.java
@@ -0,0 +1,42 @@
+package dev.andrei.chirila.lowroll.account;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "bank_name")
+    private String bankName;
+
+    public Account() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+}
